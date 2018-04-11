3a5b8d13d5699b1056e3ecccb9ad1ea1e1dd9146
dengrt <drtdrtdrt@126.com>
2018-04-11 15:09:46 +0800

    222

--- app/schedule/interfacesLogSchedule.js
+++ app/schedule/interfacesLogSchedule.js
@@ -4,7 +4,7 @@ class InterfacesLog extends Subscription {
     // 通过 schedule 属性来设置定时任务的执行间隔等配置
     static get schedule() {
         return {
-            interval: '200m', // 1 分钟间隔
+            interval: '10m', // 1 分钟间隔
             type: 'all', // 指定所有的 worker 都需要执行
         };
     }
