94eeb4678ea84272267bede445e6fefaea49aa4c
syuyuusyu <syuyuusyu@gmail.com>
2018-08-17 09:55:25 +0800

    syu

--- app/router.js
+++ app/router.js
@@ -289,10 +289,4 @@ module.exports = app => {
     //获取接口配置信息
     router.get('/interfaceConfig/:flag', controller.interfaceConfig.interfaceConfig);
 
-    //获取所有监控日志
-    router.get('/monitorLog/allMonitorLog', controller.monitorLog.getAllMonitorLog);
-    //获取所有虚拟机名称
-    router.get('/monitorLog/allInstanceName', controller.monitorLog.getAllInstanceName);
-    //根据条件查询出监控日志
-    router.post('/monitorLog/queryMonitorLog', controller.monitorLog.getQueryMonitorLog);
 };
